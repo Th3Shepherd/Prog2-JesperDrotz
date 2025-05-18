df7505391227c876622566e501ac157e453d113c
Jesper <jesper.drotz@skola.taby.se>
2025-05-18 14:47:13 +0200

    jag har gjort e-nivå hoppas jag iallafall på databasen jag kommer göra mer senare idag, men jag har en grund iallafall,det finns dock en bug vilket gör att jag inte får se resultatet och får  det kan vara nåt fel med min databas

--- databas/databas.py
+++ databas/databas.py
@@ -3,7 +3,21 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="",
-  database="Prog2"
+  database="första"
 )
 mycursor = mydb.cursor()
 print("Uppkopplad till databasen!")
+
+namn = input("Ange username att söka efter: ")
+
+sql = "SELECT * FROM users WHERE username = %s"
+mycursor.execute(sql, (namn,))
+
+resultat = mycursor.fetchall()
+
+if resultat:
+    print("Resultat:")
+    for rad in resultat:
+        print(rad)
+else:
+    print("Inget username hittades med det namnet.")
